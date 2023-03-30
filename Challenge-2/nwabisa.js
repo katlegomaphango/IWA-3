b678abeef84200d889286d4f1c4b43567e99cbe7
Katlego Maphango <mphangokatlego08@gmail.com>
2023-03-30 04:32:21 +0200

    Replaced private withconst and public with export

--- Challenge-2/nwabisa.js
+++ Challenge-2/nwabisa.js
@@ -1,8 +1,8 @@
 // nwabisa.js
 
-private firstname = "Nwabisa"
-private surname = "Gabe"
-public role = "CEO"
+const firstname = "Nwabisa"
+const surname = "Gabe"
+export const role = "CEO"
 
-private display= firstname + " " + surname + " (" + role + ")"
+const display= firstname + " " + surname + " (" + role + ")"
 document.querySelector('#nwabisa').innerText = display
