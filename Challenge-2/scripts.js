f687cc5fe6a08797e58da1dedb6af2be43fac93d
Katlego Maphango <mphangokatlego08@gmail.com>
2023-03-30 04:37:31 +0200

    Added the imports and refracted the console.log

--- Challenge-2/scripts.js
+++ Challenge-2/scripts.js
@@ -1,1 +1,5 @@
-console.log('Roles:', nwabisa.role, johanes.role, alex.role)
+import { role as nwabisa_role } from "./nwabisa.js";
+import { role as johanes_role } from "./johannes.js";
+import { role as alex_role } from "./alex.js";
+
+console.log('Roles:', nwabisa_role, johanes_role, alex_role)
